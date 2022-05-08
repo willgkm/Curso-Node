6291dfd443d84d826b1ab73008c5a0901eec058d
willian gabriel <wkuviatkovski@gmail.com>
2022-05-07 21:06:04 -0300

    projeto finalizado

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
-const http = require("http")
-const port = 3000;
+import app from "./src/app.js";
 
-const rotas = {
-  '/': 'Curso de node',
-  '/livros': 'pagina de livros',
-  '/autores': 'listagem de autores',
-  '/editora': 'pagina de editora',
-  '/sobre': 'Sobre isso',
-}
+const port = process.env.PORT || 3000;
 
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end(rotas[req.url])
-})
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log (`Servidor na porta http://localhost:${port}`)
 })
